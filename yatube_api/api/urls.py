bb6f20d5781ba6bd809b3158c96d25273b46b892
Anton Antonov <tonik32@gmail.com>
2022-03-24 15:48:21 +0300

    описывает роутер posts

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,12 @@
-from django.urls import path
+from django.urls import  include, path
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet
+
+app_name = 'api'
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
 
 urlpatterns = [
+    path('v1', include(router.urls))
 ]
