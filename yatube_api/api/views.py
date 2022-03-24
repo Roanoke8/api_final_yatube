820a67a104c8fe3a61726c354aaea4af66da321e
Anton Antonov <tonik32@gmail.com>
2022-03-24 15:01:15 +0300

    Базово описывает вьюсеты

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,21 @@
+from re import S
 from rest_framework import viewsets
 
-from posts.models import Post
-from .serializers import PostSerializer
+from posts.models import Comment, Follow, Group, Post
+from .serializers import CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+
+class CommentViewsSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
