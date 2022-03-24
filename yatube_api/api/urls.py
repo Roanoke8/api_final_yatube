cfcf53d763d1856208328066184f68d6a2e25545
Anton Antonov <tonik32@gmail.com>
2022-03-24 18:32:33 +0300

    Добавляет роутинг JWT

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import  include, path
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import GroupViewSet, PostViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
+router.register('groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
-    path('v1', include(router.urls))
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
