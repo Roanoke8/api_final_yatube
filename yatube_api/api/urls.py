159bb64ce5c9ff9a858589f54c2e081fd5bee167
Anton Antonov <tonik32@gmail.com>
2022-03-25 21:50:19 +0300

    Описывает путь для commentview

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,15 +1,16 @@
 from django.urls import  include, path
 from rest_framework.routers import DefaultRouter
-from .views import GroupViewSet, PostViewSet
+from .views import CommentViewsSet, GroupViewSet, PostViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 router.register('groups', GroupViewSet, basename='groups')
+router.register(r'posts/(?P<post_pk>\d+)/comments', CommentViewsSet, basename='comments')
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include(router.urls)),
 ]
