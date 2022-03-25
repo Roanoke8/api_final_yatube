a36d833b90f9dc052696f0c39bf3b866a667fba9
Anton Antonov <tonik32@gmail.com>
2022-03-25 21:44:56 +0300

    Меняет оператор сравнения

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,9 +5,9 @@ from rest_framework import permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """Проверяет что пользователь является автором."""
     def has_object_permission(self, request, view, obj):
-        if request.method == permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
-        
+
         if obj.author != request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
         return obj.author == request.user
