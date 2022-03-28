851aebe6b9f58a11c1b052549eeb0f6b0a8ea6b7
Anton Antonov <tonik32@gmail.com>
2022-03-28 22:01:56 +0300

    Описывает метод follow

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
+from django.contrib.auth import get_user_model
 
 from posts.models import Comment, Follow, Group, Post
 
 from rest_framework import mixins, viewsets
-from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import LimitOffsetPagination
 
 from .serializers import CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
 from .permissions import IsOwnerOrReadOnly
 
+User = get_user_model()
+
 class ListViewSet(mixins.ListModelMixin,
                   mixins.RetrieveModelMixin,
                   viewsets.GenericViewSet):
+    """TODO"""
     pass
 
 
@@ -28,23 +33,49 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class GroupViewSet(ListViewSet):
+    """TODO"""
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     pagination_class = None
 
+
 class FollowViewSet(viewsets.ModelViewSet):
+    """TODO"""
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+    
+
+    def get_queryset(self):
+        """TODO"""
+        queryset = self.request.user.follower.filter()
+        user = self.request.query_params.get('search')
+        if user is not None:
+            queryset = get_list_or_404(queryset.filter(following__username=user))
+        return queryset
+    
+    def perform_create(self, serializer):
+        """TODO"""
+        follow = get_object_or_404(User, username=self.request.data['following'])
+        serializer.save(
+            user=self.request.user,
+            following=follow
+        )
+
+    
 
 class CommentViewsSet(viewsets.ModelViewSet):
+    """TODO"""
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
     def get_queryset(self):
+        """TODO"""
         post = get_object_or_404(Post, pk=self.kwargs['post_pk'])
         return post.comments.all()
     
     def perform_create(self, serializer):
+        """TODO"""
         post = get_object_or_404(Post, pk=self.kwargs['post_pk'])
         serializer.save(author=self.request.user, post=post)
