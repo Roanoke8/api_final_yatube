95c32d6cae44dddf7e625f07c3297cfd19b46472
Anton Antonov <tonik32@gmail.com>
2022-03-28 22:01:27 +0300

    Добавляет валидацию

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
 
-from posts.models import Comment, Post, Group, Follow
+from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """TODO"""
     author = SlugRelatedField(
         slug_field='username',
         read_only=True
@@ -17,6 +19,7 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """TODO"""
     author = serializers.SlugRelatedField(
         read_only=True,
         slug_field='username'
@@ -28,17 +31,43 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
 
 class GroupSerializer(serializers.ModelSerializer):
-
+    """TODO"""
     class Meta:
         fields = '__all__'
         model = Group
 
 class FollowSerializer(serializers.ModelSerializer):
+    """TODO"""
     user = serializers.SlugRelatedField(
         read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
         slug_field='username'
     )
-    
+
+    validators = [
+        UniqueTogetherValidator(
+            queryset=Follow.objects.all(),
+            fields=('user', 'following'),
+            message='Вы уже подписанны на данного пользователя'
+        )
+    ]
+
+    def validate(self, data):
+        """TODO"""
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError(
+                'нельзя подписываться на самого себя!'
+            )
+        return data
+
+
+
     class Meta:
-        fields = '__all__'
+        fields = ('user', 'following')
         model = Follow
+        
