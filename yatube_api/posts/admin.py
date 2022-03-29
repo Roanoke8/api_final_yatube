0e6d40458f3ec349fe960091dba45bd7f416f40b
Anton Antonov <tonik32@gmail.com>
2022-03-29 20:21:20 +0300

    мелкое форматирование.

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 
-from .models import Comment, Follow, Group, Post
+from .models import Post
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author')
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
-
-
-admin.site.register(Post)
-admin.site.register(Group)
-admin.site.register(Comment)
-admin.site.register(Follow)
