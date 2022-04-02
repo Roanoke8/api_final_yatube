be9671080a49bf2cda8de409cfea4551f7bc0730
Anton Antonov <tonik32@gmail.com>
2022-04-02 21:18:32 +0300

    isort .

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
@@ -9,9 +10,6 @@ from .mixins import CreateViewSet, ListViewSet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
-
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
 
 
 class PostViewSet(viewsets.ModelViewSet):
